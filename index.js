3a0f32d731d7614c0ddb2b44a4406ec08feb2d94
audtheshaud <abookernelson@gmail.com>
2025-05-29 22:25:44 +0200

    WORKS LIKE A CHARM

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-// Import necessary constants if desired, e.g.,
-// import { SESSION_PTY_DATA } from '../../typings/constants/sessions';
+const awaiting = new Set();
 
 exports.middleware = (store) => (next) => (action) => {
   // For actions containing PTY output
-  if (action.type === 'SESSION_ADD_DATA') {
+  if (action.type === 'SESSION_PTY_DATA') {
+    console.log('RAW PTY>', JSON.stringify(action.data));
     // SESSION_PTY_DATA has a data property that contains the terminal output.
-    const { data } = action;
+    const { data, uid } = action;
     
     // For instance, check if the output includes a carriage return.
     // This might indicate that a command has just finished executing.
-    if (data.includes('\n?\r')) {
+    if (!awaiting.has(uid) && /\r?\n/.test(data)) {
+      awaiting.add(uid);
       // Dispatch an action to disable cursor animation,
       // or perform any other UI update based on the new output.
       store.dispatch({
         type: 'CURSOR_DISABLE_ANIMATION'
       });
-      
-      // Optionally, you might choose NOT to pass this action further if you want to mask the raw output.
     }
+
+    if (awaiting.has(uid) && (/\]\$\u001b\[00m/.test(data) || /[^a-zA-Z0-9]?\s*\b(?:yes|no|y\/n|y|n)\b\s*\:?$/i.test(data))) {
+      awaiting.delete(uid);
+      store.dispatch({
+        type: 'CURSOR_ENABLE_ANIMATION'
+      });
+    }
+
   }
   next(action);
 };
@@ -28,7 +35,12 @@ exports.middleware = (store) => (next) => (action) => {
 
 // This function maps Hyper's UI state (from Redux) and adds our custom
 // property "cursorAnimated". Assume the UI state is stored under state.ui.
-exports.reduceUI = (state, action) => {
+
+const defaultUIState = {
+  cursorAnimated: true  // or false, depending on your default
+};
+
+exports.reduceUI = (state = defaultUIState, action) => {
   switch (action.type) {
     case 'CURSOR_DISABLE_ANIMATION':
       return state.set('cursorAnimated', false);
@@ -84,7 +96,7 @@ exports.decorateTerm = (Term, { React }) => {
         width: '2px',           // thinner cursor
         height: '14px',         // shorter height
         background: '#8F93A2',
-        transition: 'transform 100ms ease-out',
+        transition: 'transform 80ms ease-out',
         zIndex: 1000,
         pointerEvents: 'none',
         borderRadius: '1px'
@@ -102,9 +114,12 @@ exports.decorateTerm = (Term, { React }) => {
       // Use the Redux state property "cursorAnimated" passed into props.
       // When true, animate smoothly; when false, immediately update.
       if (this.props.cursorAnimated) {
-        this.lastCursor.x = lerp(this.lastCursor.x, this.targetCursor.x, 0.2);
-        this.lastCursor.y = lerp(this.lastCursor.y, this.targetCursor.y, 0.2);
+        this.lastCursor.x = lerp(this.lastCursor.x, this.targetCursor.x, 0.3);
+        this.lastCursor.y = lerp(this.lastCursor.y, this.targetCursor.y, 0.3);
+        this.cursor.style.display = 'block';
       } else {
+        this.cursor.style.display = 'none';
+        this.cursor.style.transition = 'none';
         this.lastCursor = { ...this.targetCursor };
       }
       this.cursor.style.transform = `translate(${this.lastCursor.x}px, ${this.lastCursor.y}px)`;
